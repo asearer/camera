0abc71f25224d45e7a30261bad0e27301f12bd6d
asearer <amsearer84@gmail.com>
2023-07-15 21:16:54 -0400

    On branch master  Initial commit  Changes to be committed: 	new file:   camera.py

---
+++ camera.py
@@ -0,0 +1,23 @@
+import cv2
+
+def main():
+    # Open the default camera
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Display the resulting frame
+        cv2.imshow('Camera', frame)
+
+        # Check for 'q' key press to exit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
